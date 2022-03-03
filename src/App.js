12bc0587cd7ebebe29bd4e5bcb9576f6d6f6458c
Anirudh Sharma <anirudhsharma885@outlook.com>
2022-03-03 22:41:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import Movie from './Movie';
+import Filter from './Filter';
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from 'framer-motion';
+
+function App() {
+
+const [popular, setPopular] = useState([]);
+const [filtered, setFiltered] = useState([]);
+const [activeGenre, setActiveGenre] = useState(0);
+
+useEffect(()=>{
+  fetchPopular();
+}, []);
+
+
+  const fetchPopular = async ()=>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f3878435dc27a0c5a4bb6f92eb596acc&language=en-US&page=1');
+    const movies = await data.json();
+
+    setPopular(movies.results);
+    setFiltered(movies.results);
+
+  }
+
+  return (
+    <div className="App">
+      <Filter popular={popular} setFiltered={setFiltered} activeGenre={activeGenre} setActiveGenre={setActiveGenre}/>
+      <motion.div
+      layout={true} 
+       className='popular-movies'>
+         <AnimatePresence>
+        {filtered.map(movie => {
+          return <Movie key={movie.id} movie={movie}/>
+        })}
+         </AnimatePresence>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
